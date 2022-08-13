29f4833d8e77dbd23b2202242efcaf5d467c5ff5
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-08-13 14:07:55 -0600

    Fixed all Bugs
    Can now successfully build

--- Factory/Models/FactoryContext.cs
+++ Factory/Models/FactoryContext.cs
@@ -4,8 +4,8 @@ namespace Factory.Models
 {
   public class FactoryContext : DbContext
   {
-    public DbSet<Engineer> Categories { get; set; }
-    public DbSet<Item> Items { get; set; }
+    public DbSet<Engineer> Engineers { get; set; }
+    public DbSet<Machine> Machines { get; set; }
     public DbSet<EngineerMachine> EngineerMachine { get; set; }
 
     public FactoryContext(DbContextOptions options) : base(options) { }
